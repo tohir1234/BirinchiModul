f34e007b4192a4437db6b2c5abc829696dd11d37
tohir1234 <tohirdosmatov60@gmail.com>
2025-12-03 15:04:06 +0500

    birinchi

--- Dars1/dars1/dars1/Program.cs
+++ Dars1/dars1/dars1/Program.cs
@@ -4,7 +4,204 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tohir Dosmatov");
+
+            /*1
+            Console.WriteLine("sonni kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int natija = son % 10;
+            Console.WriteLine("Birlar xonasidagi son" + natija);*/
+
+            /*2
+           Console.WriteLine("uch xonali son kiriting");
+           int son = int.Parse(Console.ReadLine());
+           int natija = son / 10 %10;
+           Console.WriteLine("Onlar xonasidagi son=" +  natija);*/
+
+            /*3
+            Console.WriteLine("uch xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int natija = son /10 / 10 %10;
+            Console.WriteLine("yuzlar xonasidagi son=" +  natija);*/
+
+            /*4
+            Console.WriteLine("besh xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int natija = son /10 /10 / 10 %10;
+            Console.WriteLine("minglar xonasidagi son=" +  natija);*/
+
+            /*5
+            Console.WriteLine("tort xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int natija = son  /10 / 10 %10;
+            Console.WriteLine("minglar xonasidagi son=" +  natija);*/
+
+            /*6
+            Console.WriteLine("uch xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int natija = son %10;
+            int natija1 = son/10 %10;
+            int natija2 = son /10/10;
+            Console.WriteLine("raqamlar yogindisi=" +  (natija+natija1+natija2));*/
+
+            /*7
+            Console.WriteLine("uch xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int natija = son %10;
+            int natija1 = son/10 %10;
+            int natija2 = son /10/10;
+            Console.WriteLine("raqamlar kopaytmasi=" +  (natija*natija1*natija2));*/
+
+            /*8
+            Console.WriteLine("ikki xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int natija = son %10;
+            int natija1 = son/10 %10;
+           
+            Console.WriteLine("raqamlar kopaytmasi=" +  (natija*natija1));*/
+
+            /*9
+            Console.WriteLine("besh xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int natija = son %10;
+            int natija2 = son/10 %10;
+            int natija3 = son/10/10 %10;
+            int natija4 = son/10/10/10 %10;
+            int natija5 = son/10/10/10/10 %10;
+           
+            Console.WriteLine("raqamlar kopaytmasi=" +  (natija+natija2+natija3+natija4+natija5));*/
+
+            /*10
+            Console.WriteLine("uch xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int a = son /100;
+            int b = (son / 10) % 10;
+            int c = son % 10;
+            int temp = c * 100 + b * 10 + a;
+            
+           
+            Console.WriteLine(temp );*/
+
+            /*11
+            Console.WriteLine("tort xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int a = son / 1000;
+            int b = (son / 100) % 10;
+            int c = son /10 % 10;
+            int d = son % 10;
+            int temp =a*1000+b*100+ d* 10 + c;
+            
+           
+            Console.WriteLine(temp );*/
+
+            /*12
+             Console.WriteLine("uch xonali son kiriting");
+             int son = int.Parse(Console.ReadLine());
+
+             int b = (son / 100) % 10;
+             int c = son /10 % 10;
+             int d = son % 10;
+             int temp = c * 100 + b * 10 + d;
+
+
+             Console.WriteLine(temp );*/
+
+            /*13
+            Console.WriteLine("tort xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int a = son / 1000;
+            int b = (son / 100) % 10;
+            int c = son /10 % 10;
+            int d = son % 10;
+            int temp = c * 1000+b*100+a*10+d;
+            
+           
+            Console.WriteLine(temp );*/
+
+            /*14
+             Console.WriteLine("GB kiriting");
+             int son = int.Parse(Console.ReadLine());
+
+             int temp = son * 1024; 
+
+
+             Console.WriteLine("MB="+temp );*/
+
+            /*15
+            Console.WriteLine("GB kiriting");
+            long son = int.Parse(Console.ReadLine());
+
+            long temp = son * 1024 *1024 * 1024 * 8; 
+            
+           
+            Console.WriteLine("BIT="+temp );*/
+
+            /*16
+            Console.WriteLine("GB  kiriting");
+            long son = int.Parse(Console.ReadLine());
+
+            long temp = son /1024; 
+            
+           
+            Console.WriteLine("TB="+temp );*/
+
+            /*17
+            Console.WriteLine("GB  kiriting");
+            long son = int.Parse(Console.ReadLine());
+
+            long temp = son /2; 
+            
+           
+            Console.WriteLine("kinolar soni="+temp );*/
+
+            /*18
+            Console.WriteLine("GB  kiriting");
+            long son = int.Parse(Console.ReadLine());
+
+            long temp = son /5; 
+            
+           
+            Console.WriteLine("kinolar soni="+temp );*/
+
+            /*19
+            Console.WriteLine("uch xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int a = son / 100;
+            int b = (son / 10) % 10;
+            int c = son % 10;
+            int temp = b * 100 + c * 10 + a;
+
+
+            Console.WriteLine(temp);*/
+
+            /*20
+            Console.WriteLine("uch xonali son kiriting");
+            int son = int.Parse(Console.ReadLine());
+            int a = son / 100;
+            int b = (son / 10) % 10;
+            int c = son % 10;
+            int temp = b * 100 +a * 10 + c;
+
+
+            Console.WriteLine(temp);*/
+
+            /*21
+
+            Console.WriteLine("uch xonali son kiriting");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int result = n >> 2;   
+
+            Console.WriteLine(result);*/
+            /*22
+
+            Console.WriteLine("uch xonali son kiriting");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int result = n << 3;   
+
+            Console.WriteLine(result);*/
         }
     }
 }
