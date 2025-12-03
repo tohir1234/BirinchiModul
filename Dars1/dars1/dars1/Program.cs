f0f761e89c4b48392203ae8350c1a1c23961faf7
tohir1234 <tohirdosmatov60@gmail.com>
2025-12-03 16:05:16 +0500

    uchinchi'

--- Dars1/dars1/dars1/Program.cs
+++ Dars1/dars1/dars1/Program.cs
@@ -202,6 +202,215 @@
             int result = n << 3;   
 
             Console.WriteLine(result);*/
+//       4- Homework
+
+            /*1
+           Console.WriteLine("son kiriting ");
+           int son=int.Parse(Console.ReadLine());
+         int natija=son*son;
+           int daraja=natija*natija;
+
+           Console.WriteLine(natija*/
+
+            /*2
+            Console.WriteLine("son kiriting ");
+            int son=int.Parse(Console.ReadLine());
+            int daraja = son * son;
+            int kub = daraja * son;
+            int daraja6 = kub * kub;
+
+            Console.WriteLine(daraja6);*/
+
+            /*3
+            Console.WriteLine("son kiriting ");
+            int son=int.Parse(Console.ReadLine());
+            int daraja2 = son * son;
+            int daraja4 = daraja2 * daraja2;
+            int daraja8 = daraja4 * daraja4;
+
+            Console.WriteLine(daraja8);*/
+
+            /*4
+            Console.WriteLine("soatni kiriting ");
+            int son = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("kunni kiriting ");
+            int kun = int.Parse(Console.ReadLine());
+            int a = kun * 24;
+            int b = son + a;
+            Console.WriteLine(b);*/
+            /*5
+            Console.WriteLine("haftani kiriting ");
+            int son = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("soatni kiriting ");
+            int kun = int.Parse(Console.ReadLine());
+
+            int hafta = son * 7;
+            double soat = kun / 24.0;
+            double   m = hafta + soat;
+            Console.WriteLine(m); */
+
+            /*6
+            Console.WriteLine("minutni kiriting ");
+            int son = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("soatni kiriting ");
+            int kun = int.Parse(Console.ReadLine());
+
+            int hafta = son* 60;
+            double soat = kun*60*60;
+            double   m = hafta + soat;
+            Console.WriteLine("sekund="+m);*/
+            /*7
+            Console.WriteLine("baytni kiriting ");
+            int bayt = int.Parse(Console.ReadLine());
+            int kilo = bayt / 1024;
+            Console.WriteLine("KB=" + kilo);*/
+            /*8
+            double tezlik = 750;
+            double hajmGb = 1.8;
+            double hajmKb = hajmGb * 1024 * 1024;
+            double hajmsekund = hajmKb / tezlik;
+
+            Console.WriteLine("fayl uzatish vaqti" + hajmsekund+"sekund");*/
+            /*9
+            Console.WriteLine("dollarni  kiriting ");
+            int dol = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("yevroni kiriting ");
+            int yev = int.Parse(Console.ReadLine());
+            int a = dol * 12600;
+            int b = yev * 14500;
+            int c = a + b;
+            Console.WriteLine("somga aylantirish--" + c);*/
+            /*10
+             Console.WriteLine("sonni  kiriting "); 
+            int son = int.Parse(Console.ReadLine());
+           
+           
+            int darja2 = son * son;
+            int darja4 = darja2 * darja2;
+            int darja8 = darja4 * darja4;
+            int darja10 = darja8 * darja2;
+            Console.WriteLine("10-chi daraja" + darja10);
+            */
+            /*11
+            int sahifa = 720;
+            int qator = 50;
+            int belgi = 152;
+            long umumiy = sahifa * qator * belgi;
+            double hajm = umumiy / (1024.0 * 1024.0);
+            Console.WriteLine("elektron kitob hajmi" + hajm+"MB");*/
+            /*12
+            Console.WriteLine("sonni  kiriting ");
+            double n = int.Parse(Console.ReadLine());
+            double belgi = n * 2.5;
+            double soz = belgi / 5;
+            Console.WriteLine("Akmal taxminan 2,5 daqiqada--" + soz + "soz yozgan");*/
+            /*13
+            Console.WriteLine("1- sonni  kiriting ");
+            double a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2- sonni  kiriting ");
+            double b = int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("almashtirilgandan keyin");
+            Console.WriteLine("a="+a);
+            Console.WriteLine("b="+b); */
+            /*14
+            Console.WriteLine("1- sonni  kiriting ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2- sonni  kiriting ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("3- sonni  kiriting ");
+            int  c = int.Parse(Console.ReadLine());
+            int temp = a;
+            a = b;
+            b = c;
+            c = temp;
+            Console.WriteLine("Almashtirilgandan keyin");
+            Console.WriteLine("a="+a);
+            Console.WriteLine("b="+b);
+            Console.WriteLine("c=" + c);*/
+            /*15
+            Console.WriteLine("Patrikni pulini kiriting ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Julianing pulini kirting");
+            int b = int.Parse(Console.ReadLine());
+            if (a <= b)
+            {
+                Console.Write("Patrikning puli Julianikidan kop bolish kerak");
+                return;
+            }
+            double d = (a - b) / 2;
+            Console.Write($"Patrik Juliaga  {d}  shunch som berish kerak");*/
+            /*16
+            Console.WriteLine("km ni kiriting ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("metr ni kirting");
+            int b = int.Parse(Console.ReadLine());
+            double c = a * 1000 + b * 10;
+            Console.Write($"Jami {c}  dm boladi");*/
+
+            /*17
+            Console.WriteLine("15 chi darajani chiqaring ");
+            int son = int.Parse(Console.ReadLine());
+            int d1= son * son;
+            int d3=d1* son;
+            int d6 = d3 * d3;
+            int d12 = d6 * d6;
+            int d15 = d12 * d3;
+            Console.Write($"15 daraja  {d15}");*/
+            /*18
+            Console.WriteLine("binoni kiriting(a)");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("qavatni kiriting(b)");
+            int b= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("xonani kiriting(c)");
+            int c= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("stulni kiriting(d)");
+            int d= int.Parse(Console.ReadLine());
+            int n = a * b * c * d;
+            int m = (n + 1) / 2;
+            Console.WriteLine();
+            Console.Write($"Jami stulllar soni {n}");
+            Console.WriteLine();
+            Console.Write($"kerakli  partalar soni {m}");*/
+
+            /*19
+            Console.WriteLine("soatni kiriting'");
+            int soat = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("minutni kiritng");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("sekundni kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+         double sekund = soat * 60 + m + a / 60.0;
+            Console.Write($"jami minut {sekund}");*/
+
+
+            /*20
+                 double x, y, a, b;
+                 x = double.Parse(Console.ReadLine());
+                 a = double.Parse(Console.ReadLine());
+
+                 y = double.Parse(Console.ReadLine());
+                 b = double.Parse(Console.ReadLine());
+
+                 double result = (b / y) - (a / x);
+                 Console.WriteLine(result);*/
         }
     }
 }
