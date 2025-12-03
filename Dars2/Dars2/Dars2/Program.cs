0fdf39db926fcd64ed2c4f5f74f7e2087607f426
tohir1234 <tohirdosmatov60@gmail.com>
2025-12-03 23:02:57 +0500

    5chiHome

---
+++ Dars2/Dars2/Dars2/Program.cs
@@ -0,0 +1,484 @@
+﻿namespace Dars2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5- homework");
+
+            /*1
+         Console.WriteLine("son kiriting");
+         int n = int.Parse(Console.ReadLine());
+         if (n % 2 == 0)
+         {
+             Console.WriteLine("berilgan son juft");
+         }
+         else
+         {
+             Console.WriteLine("berilgan son toq");
+         }*/
+            /*2
+            Console.WriteLine("son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 != 0)
+            {
+                Console.WriteLine("berilgan son toq");
+            }
+            else
+            {
+                Console.WriteLine("berilgan son juft");
+            }*/
+
+            /*3
+             Console.WriteLine("son kiriting");
+             int n = int.Parse(Console.ReadLine());
+             if (n >=10 && n<=99)
+             {
+                 Console.WriteLine("berilgan son 2 xonali");
+             }
+             else
+             {
+                 Console.WriteLine("berilgan son notogri");
+             }*/
+            /*4
+             Console.WriteLine("son kiriting");
+             int n = int.Parse(Console.ReadLine());
+             if (n >999 && n<=9999)
+             {
+                 Console.WriteLine("berilgan son 4 xonali");
+             }
+             else
+             {
+                 Console.WriteLine("berilgan son notogri");
+             }*/
+            /*5
+             Console.WriteLine("son kiriting");
+             int n = int.Parse(Console.ReadLine());
+             if (n%2==0&& n>=10&& n<=99)
+             {
+                 Console.WriteLine("berilgan son juft va 2 xonali");
+             }
+             else
+             {
+                 Console.WriteLine("berilgan son notogri");
+             }*/
+
+            /*6
+            Console.WriteLine("son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            if (n%2!=0&& n>99&& n<=999)
+            {
+                Console.WriteLine("berilgan son toq va 3 xonali");
+            }
+            else
+            {
+                Console.WriteLine("berilgan son notogri");
+            }*/
+
+            /*7
+            Console.WriteLine("son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0 && n > 999 && n <= 9999)
+            {
+                Console.WriteLine("berilgan son juft va 4 xonali");
+            }
+            else
+            {
+                Console.WriteLine("berilgan son notogri");
+            }*/
+            /*8
+            Console.WriteLine("son kiriting");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("son kiriting");
+            int m = int.Parse(Console.ReadLine());
+            if (n % 2 != 0&&m%2!=0 )
+            {
+                Console.WriteLine("berilgan son ikkalasi ham toq");
+            }
+            else
+            {
+                Console.WriteLine("berilgan son notogri");
+            }*/
+            /*9
+           Console.WriteLine("son kiriting");
+           int n = int.Parse(Console.ReadLine());
+
+           Console.WriteLine("son kiriting");
+           int m = int.Parse(Console.ReadLine());
+           if (n % 2 == 0 && m % 2 == 0)
+           {
+               Console.WriteLine("berilgan son ikkalasi ham juft");
+           }
+           else
+           {
+               Console.WriteLine("berilgan son notogri");
+           }*/
+            /*10
+            Console.WriteLine("son kiriting");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("son kiriting");
+            int m = int.Parse(Console.ReadLine());
+            if (n % 2 != 0 && m % 2 != 0 && n>=10 && n<=99 && m>=10 && m<=99)
+            {
+                Console.WriteLine("berilgan son ikkalasi ham toq va 2 xonali");
+            }
+            else
+            {
+                Console.WriteLine("berilgan son notogri");
+            }*/
+            /*11
+            Console.WriteLine("son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 9 || n >= 99)
+            {
+                Console.WriteLine("iltimos faqat ikki xonali son kiriting");
+                    return;
+            }
+            int onlik = n / 10;
+            int birlik = n % 10;
+            if(onlik== birlik)
+            {
+                Console.WriteLine("son raqamlari teng ");
+
+            }
+            else
+            {
+                Console.WriteLine("son raqamlari teng emas");
+            }*/
+
+            /*12
+            Console.WriteLine("ikki xonali son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            int m = n % 10;
+            int c = n / 10;
+            int d = m + c;
+            if (10 < d)
+            {
+                Console.Write("10 dan kotta soon");
+            }
+            else
+            {
+                Console.Write("boshqa son kiritng");
+
+            }
+            return;*/
+            /*13
+            Console.WriteLine("ikki xonali son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            int m = n % 10;
+            int c = n / 10;
+            int d = m * c;
+            if (10 < d)
+            {
+                Console.Write("20 dan kotta soon");
+            }
+            else
+            {
+                Console.Write("boshqa son kiritng");
+
+            }
+            return;*/
+
+            /*14
+            Console.WriteLine("ikki xonali son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            int m = n % 10;
+            int c = n / 10;
+
+            if (m % 2 != 0 && c % 2 != 0) 
+            {
+                Console.Write("ikki son ham toq");
+            }
+            else
+            {
+                Console.Write("boshqa son kiritng");
+
+            }
+            return;*/
+            /*15
+            Console.WriteLine("ikki xonali son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            int m = n % 10;
+            int c = n / 10;
+
+            if (m >5 && c>5) 
+            {
+                Console.Write("ikki son ham 5dan kotta");
+            }
+            else
+            {
+                Console.Write("boshqa son kiritng");
+
+            }
+            return;*/
+
+            /*16
+            Console.WriteLine("ikki xonali son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            if (n % 3 == 0)
+            {
+                Console.Write("3 ga karrali son");
+            }
+            else
+            {
+                Console.Write("3 ga kkarali emas");
+            }
+            */
+            /*17
+            Console.WriteLine("ikki xonali son kiriting");
+            int n = int.Parse(Console.ReadLine());
+            if (n % 7 == 0)
+            {
+                Console.Write("7 ga karrali son");
+            }
+            else
+            {
+                Console.Write("7 ga kkarali emas");
+            }*/
+            /*18
+            Console.WriteLine("ikki xonali son kiriting");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ikki xonali son kiriting");
+            int m = int.Parse(Console.ReadLine());
+            if (n % 3 == 0 && m%8==0)
+            {
+                Console.Write("3 ga 8 ga karrali son");
+            }
+            else
+            {
+                Console.Write("3 ga 8 ga kkarali emas");
+            }*/
+            /*19
+            Console.WriteLine("ikki xonali son kiriting");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ikki xonali son kiriting");
+            int m = int.Parse(Console.ReadLine());
+            if (n % 7 == 0 && m%9==0)
+            {
+                Console.Write("7 ga 9 ga karrali son");
+            }
+            else
+            {
+                Console.Write("7 ga 9 ga kkarali emas");
+            }*/
+            /*20
+             Console.WriteLine("a sonini kiriting");
+             int a = int.Parse(Console.ReadLine());
+             if (a % 3 == 0)
+             {
+                 Console.Write("Rost:a soni 3ga karrali");
+
+             }
+             else
+             {
+                 Console.Write("Yolgon:a soni 3ga karrali emas");
+             }
+            */
+
+            /*21
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+            if (a % 5 != 0)
+            {
+                Console.WriteLine("Rost:a soni 5ga bolinmaydi");
+
+            }
+            else
+            {
+                Console.WriteLine("Yolgon:????");
+            }*/
+            /*22
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+            if (a % 4 == 0 || a % 3 != 0)
+            {
+                Console.WriteLine("Rost:4 ga bolinadi va 3ga bolinmaydi");
+            }
+            else
+            {
+                Console.WriteLine("yolgon ????");
+            }*/
+            /*23
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Rost:a soni b sonidan katta");
+            }
+            else
+            {
+                Console.Write("Yolgon:a soni b sonidan katta emas");
+            }*/
+            /*24
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+            if (a == b)
+            { 
+                Console.WriteLine("Rost:a soni b soniga teng");
+            }
+            else
+            {
+                Console.Write("Yolgon:a soni b soniga teng emas");
+            }*/
+            /*25
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("c sonini kiriting");
+            int c = int.Parse(Console.ReadLine());
+            if (a > c && a > b)
+            {
+                Console.Write("Rost: a soni eng kottasi");
+            }
+            else
+            {
+                Console.Write("yolgon");
+            }*/
+            /*26
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("c sonini kiriting");
+            int c = int.Parse(Console.ReadLine());
+            if (a < c && b<c)
+            {
+                Console.Write("Rost: c soni eng kottasi");
+            }
+            else
+            {
+                Console.Write("yolgon");
+            }*/
+            /*27
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("c sonini kiriting");
+            int c = int.Parse(Console.ReadLine());
+            if (a < b && b>c)
+            {
+                Console.Write("Rost: b soni eng kottasi");
+            }
+            else
+            {
+                Console.Write("yolgon");
+            }*/
+            /*28
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+            if (a > 2*b)
+            {
+                Console.Write("Rost:b soni a sonidan 2 barobar kotta");
+            }
+            else
+            {
+                Console.Write("Yolgon");
+            }*/
+            /*29
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+            if (b>5*a)
+            {
+                Console.Write("Rost:b soni a sonidan 2 barobar kotta");
+            }
+            else
+            {
+                Console.Write("Yolgon");
+            }*/
+            /*30
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+            if (a>b*4&& b>a*4)
+            {
+                Console.Write("Rost:b soni a sonidan 2 barobar kotta");
+            }
+            else
+            {
+                Console.Write("Yolgon");
+            }   */
+            /*31
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+            if (a>b+30)
+            {
+                Console.Write("Rost:b soni a sonidan 2 barobar kotta");
+            }
+            else
+            {
+                Console.Write("Yolgon");
+            }*/
+
+            /*32
+            Console.WriteLine("a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+                Console.WriteLine(" c sonini kiriting");
+            int c = int.Parse(Console.ReadLine());
+                Console.WriteLine("d sonini kiriting");
+            int d = int.Parse(Console.ReadLine());
+
+            if (b > a && b > c && b > d && b % 2 == 0)
+            {
+                Console.Write("Rost:b soni eng katta va juft");
+            }
+            else
+            {
+                Console.Write("Yolgon");
+            }*/
+
+            /*33
+                        Console.WriteLine("a sonini kiriting");
+                        int a = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("b sonini kiriting");
+                        int b = int.Parse(Console.ReadLine());
+                            Console.WriteLine(" c sonini kiriting");
+                        int c = int.Parse(Console.ReadLine());
+                            Console.WriteLine("d sonini kiriting");
+                        int d = int.Parse(Console.ReadLine());
+
+                        if (a>c&&b>c&&d>c&&c%2==0)
+                        {
+                            Console.Write("Rost:c soni eng kichigi va juft");
+                        }
+                        else
+                        {
+                            Console.Write("Yolgon");
+                        }
+
+                        */
+        }
+    }
+}
