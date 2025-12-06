62817f378399a35a1b28f181b4d2813321f6704f
tohir1234 <tohirdosmatov60@gmail.com>
2025-12-06 15:03:53 +0500

    3-chinchi

---
+++ Dars3/dars3/dars3/Program.cs
@@ -0,0 +1,405 @@
+﻿namespace dars3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.3.1
+            //for (int i=1; i<=20; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            //1.3.2
+            //for (int i=25; i<=100; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+
+            //1.3.3
+            //for (int i=12; i<=200; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            //1.3.4
+            //for (int i=15; i>=2; i--)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            //1.3.5
+            //for (int i=120; i>=23; i--)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            //1.3.6
+            //for (int i=10; i<=50; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+
+            //1.3.7
+            //for (int i=120; i<=250; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+
+            //1.3.8
+            //for (int i=100; i>=50; i--)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+
+            //1.3.9
+            //for (int i=80; i>=20; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+
+            //1.3.10
+            //Console.Write(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write(" b sonini kiriting");
+            //int b = int.Parse(Console.ReadLine());
+            //for (int i=a; i<=b; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.Write( i   +  " ");
+            //    }
+            //}
+
+            //1.3.11
+            //Console.Write(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write(" b sonini kiriting");
+            //int b = int.Parse(Console.ReadLine());
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 4 == 0 && i % 7 ==0)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+
+            //1.3.12
+            //Console.Write(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write(" b sonini kiriting");
+            //int b = int.Parse(Console.ReadLine());
+            //int c = 0;
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if ( i % 8 ==0)
+            //    {
+            //        c++;
+            //    }
+            //}
+            //Console.Write($"miqdori {c}");
+
+            // 1.3.13
+            //Console.Write(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write(" b sonini kiriting");
+            //int b = int.Parse(Console.ReadLine());
+
+
+            //for (int i = b; i >= a; i--)
+            //{
+            //    if ( i %9  ==0 && i % 5==0)
+            //    {
+            //          Console.WriteLine(i + " "); 
+            //    }
+            //}
+
+            //1.3.14
+            //int c = 0;
+            //for (int i=1; i<=100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        c += i;
+            //    }
+            //}
+            //Console.WriteLine(c);
+
+
+            //1.3.15
+            //for (int i = 100; i <= 999; i++)
+            //{
+            //    int a = i / 100;        // yuzlar xonasi
+            //    int b = (i / 10) % 10;  // o'nlar xonasi
+            //    int c = i % 10;         // birliklar xonasi
+
+            //    int sum = a + b + c;
+
+            //    if (sum > 20)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+
+            //1.3.16
+            //for (int i=100; i<=999; i++)
+            //{
+            //    int r1 = i / 100;
+            //    int r2 = i / 10 % 10;
+            //    int r3 = i % 10;
+            //    if (r1 == r3)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+            //1.3.17
+            //int res = 0;
+            //int sum = 0;
+
+            //for (int i = 10; i <= 99; i++)
+            //{
+
+            //    int b = (i / 10) % 10;
+            //    int c = i % 10;
+
+            //    int p = b * c;
+
+            //    if (p > 12)
+            //    {
+            //        res++;
+            //        sum += i;
+            //  Console.WriteLine(sum + " ");
+            //    }
+            //}
+            //Console.WriteLine("miqdiri" + res);
+
+            //1.3.18
+            //for (int i = 100; i <= 999; i++)
+            //{
+
+
+
+
+            //  int b = (i / 10) % 10;
+
+            //    if ( i % 2 ==0|| i %5 ==0)
+            //    {
+
+            //        Console.WriteLine(i + " ");
+            //    }
+            //}
+
+            //1.3.19
+            //Console.WriteLine(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= a; i++)
+            //{
+
+            //    Console.Write(a + " ");
+
+            //}
+
+
+            //1.3.20
+            //Console.WriteLine(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(" b sonini kiriting");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for (int i=b; i<=a; i++)
+            //{
+
+            //    Console.Write(a + " ");
+            //}
+
+
+            //1.3.21
+            //Console.WriteLine(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(" b sonini kiriting");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for (int i=a; i<=b; i++)
+            //{
+
+            //    Console.Write( b + " ");
+            //}
+
+            //1.3.22
+            //Console.WriteLine(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+
+            //for (int i=1; i<=a; i++)
+            //{
+            //    if (a % i == 0)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+
+            //1.3.23
+            //Console.WriteLine(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+
+            //for (int i=1; i<=a; i++)
+            //{
+            //    if (a % i != 0)
+            //    {
+            //        Console.WriteLine ($" miqdori" +  i   );
+            //    }
+            //}
+
+            //1.3.23
+            //Console.WriteLine(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //int count = 0;
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    if (a % i == 0)
+            //    {
+            //        count++;
+
+            //    }
+            //}
+
+            //1.3.24
+            //Console.WriteLine(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //int count = 0;
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    if (a % i == 0)
+            //    {
+            //        count += i;
+
+            //    }
+            //}
+            //Console.Write($" yigindi  " + count);
+
+
+            //1.3.25
+            // Console.WriteLine(" a sonini kiriting");
+            // int a = int.Parse(Console.ReadLine());
+            // bool tup = true;
+            //if (a<=1)
+            // {
+            //     tup = false;
+            // }
+            // else
+            // {
+
+            //     for (int i = 2; i < a; i++)
+            //     {
+            //       if (a % i == 0)
+            //         {
+            //             tup = false;
+            //             break;
+            //         }
+            //     }
+            // }
+            // Console.WriteLine(tup);
+
+            //1.3.26
+            //Console.WriteLine(" a sonini kiriting");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(" b sonini kiriting");
+            //int b = int.Parse(Console.ReadLine());
+            //bool tup = true;
+            //int sum = 0;
+            //int sum1 = 0;
+            //if (a<=1&& b <= 1)
+            //{
+            //       tup = false;
+
+            //sum = a + b;
+            //Console.WriteLine(sum);
+            //}
+            //else
+            //{
+            //    for (int i=2; i<=a; i++)
+            //    {
+            //        if (a % i == 0)
+            //        {
+            //            tup = false;
+            //            break;
+            //        }
+            //    }
+            //    sum1 = a * b;
+            //    Console.WriteLine(sum1);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+            //1.3.28
+            //Console.WriteLine(" a sonini kiriting");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 0;
+            //for (int i = 23; i <= n; i++)
+            //{
+            //    if (i % 3 != 0)
+            //    {
+            //        count++;
+            //        Console.WriteLine(i + " ");
+            //    }
+
+            //}
+            //Console.Write($"miqdori {count}");
+
+
+
+            Console.WriteLine(" a sonini kiriting");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" b sonini kiriting");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=a; i++)
+            {
+                if (a%i == 0)
+                {
+
+                }
+            }
+
+        }
+    }
+}
