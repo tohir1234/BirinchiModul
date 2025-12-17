d70d37955069d97762109590bf0a471a6d61b6b5
tohir1234 <tohirdosmatov60@gmail.com>
2025-12-17 21:23:03 +0500

    CRUD

---
+++ Dars9/CRUD/CRUD/Program.cs
@@ -0,0 +1,141 @@
+﻿
+internal class Program
+{
+    
+    static List<string> Ids = new List<string>();
+    static void Main(string[] args)
+    {
+        var newId = string.Empty;
+        var oldId = string.Empty;
+        while (true)
+        {
+            Console.WriteLine("1. Add id");
+            Console.WriteLine("2. Delete id");
+            Console.WriteLine("3. Display ids");
+            Console.WriteLine("4. Update id");
+            Console.WriteLine("5. Quit");
+            Console.Write("Kiriting : ");
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.Write("Id ni kiriting : ");
+                newId = Console.ReadLine();
+                var res = ValidateId(newId);
+                var exists = Ids.Contains(newId);
+                if (res == true && exists == false)
+                {
+                    AddId(newId);
+                    Console.WriteLine("Id muvaffaqiyatli qo'shildi.");
+                }
+                else
+                {
+                    Console.WriteLine("Id noto'g'ri formatda kiritildi.");
+                }
+            }
+            else if (choice == 2)
+            {
+                Console.Write("Id ni kiriting : ");
+                newId = Console.ReadLine();
+                var exists = Ids.Contains(newId);
+                if (exists == true)
+                {
+                    DeleteId(newId);
+                    Console.WriteLine("Id muvaffaqiyatli o'chirildi.");
+                }
+                else
+                {
+                    Console.WriteLine("Bunday id mavjud emas.");
+
+                }
+            }
+            else if (choice == 3)
+            {
+                DisplayIds();
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Eski id ni kiriting : ");
+                oldId = Console.ReadLine();
+                Console.Write("Yangi id ni kiriting : ");
+                newId = Console.ReadLine();
+                var validateRes = ValidateId(newId);
+                var exists = Ids.Contains(oldId);
+
+                if (validateRes == true && exists == true)
+                {
+                    UpdateId(oldId, newId);
+                    Console.WriteLine("Id muvaffaqiyatli yangilandi.");
+                }
+                else
+                {
+                    Console.WriteLine("Id yangilashda xatolik yuz berdi.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Davay");
+                break;
+            }
+
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+
+
+    }
+
+    static void AddId(string id)
+    {
+        Ids.Add(id);
+    }
+
+    static void DeleteId(string id)
+    {
+        Ids.Remove(id);
+    }
+
+    static void DisplayIds()
+    {
+        foreach (var id in Ids)
+        {
+            Console.WriteLine(id);
+        }
+        Console.WriteLine();
+    }
+
+    static void UpdateId(string oldId, string newId)
+    {
+        var index = Ids.IndexOf(oldId); // AE4557789
+        if (index != -1)
+        {
+            Ids[index] = newId;
+        }
+    }
+
+    static bool ValidateId(string id)
+    {
+        if (id.Length != 9) // "AeR4578855"
+        {
+            return false;
+        }
+
+        if (!char.IsUpper(id[0]) || !char.IsUpper(id[1]))
+        {
+            return false;
+        }
+
+        for (int i = 2; i < id.Length; i++)
+        {
+            if (!char.IsDigit(id[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
